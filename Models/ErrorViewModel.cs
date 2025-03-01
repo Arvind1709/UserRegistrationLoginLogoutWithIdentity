4edf42d8b18b05ca9a30b801fbe7eebd53e3adfa
Arvind1709 <arvindyadav1709@gmail.com>
2025-03-01 22:06:05 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserRegistrationLoginLogoutWithIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
